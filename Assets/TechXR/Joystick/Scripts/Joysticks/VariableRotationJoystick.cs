331df6d3d984fc842b5f8d7f890e97119c1c7eaa
YESVIN28 <106122143@nitt.edu>
2024-11-17 13:42:46 +0530

    Initial commit: Uploading documents

---
+++ Assets/TechXR/Joystick/Scripts/Joysticks/VariableRotationJoystick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableRotationJoystick : Joystick
+{
+    #region PUBLIC_MEMBERS
+    #endregion // PUBLIC_MEMBERS
+
+    #region PRIVATE_MEMBERS
+    #endregion // PRIVATE_MEMBERS
+
+    #region MONOBEHAVIOUR_METHODS
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start(); // Calls the base class's Start method, if any
+        // Your additional initialization code here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion // MONOBEHAVIOUR_METHODS
+
+    #region PRIVATE_METHODS
+    #endregion // PRIVATE_METHODS
+
+    #region PUBLIC_METHODS
+    #endregion // PUBLIC_METHODS
+}
